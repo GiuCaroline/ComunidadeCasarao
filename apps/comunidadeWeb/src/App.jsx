e0bc2d13c8c33ab45a8bef4cfbd5e3eb5dafcf88
GiuCaroline <giuliacclaro@gmail.com>
2026-02-12 22:02:50 -0300

    Add: Adicionas dependencias para web

---
+++ apps/comunidadeWeb/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black min-h-screen text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Agora vai 😎</h1>
+    </div>
+  )
+}
+
+export default App
